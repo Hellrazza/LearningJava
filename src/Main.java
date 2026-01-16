86b53eed27a53d2d45d53604ab0baff0c4db6b84
Ryanrb6190 <91529855+Ryanrb6190@users.noreply.github.com>
2026-01-16 12:33:12 +0000

    Inputs!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a message: ");
+        String message = scanner.nextLine();
+        System.out.println(message);
     }
 }
